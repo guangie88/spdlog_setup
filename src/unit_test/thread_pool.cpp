ca7ccadc2d5387fa780a44f531c47ecf41738923
Chen Weiguang <chen.weiguang@gmail.com>
2019-11-02 02:33:43 +0800

    Add test cases for localized thread pool

--- src/unit_test/thread_pool.cpp
+++ src/unit_test/thread_pool.cpp
@@ -20,3 +20,37 @@ TEST_CASE("Parse global thread pool", "[parse_global_thread_pool]") {
     REQUIRE(original != modified);
     REQUIRE(thread_pools.empty());
 }
+
+TEST_CASE("Parse simple thread pool", "[parse_simple_thread_pool]") {
+    const auto thread_pools = spdlog_setup::details::setup_thread_pools(
+        generate_simple_thread_pool());
+
+    REQUIRE(thread_pools.size() == 1);
+}
+
+TEST_CASE(
+    "Parse invalid no name thread pool",
+    "[parse_invalid_no_name_thread_pool]") {
+    REQUIRE_THROWS_AS(
+        spdlog_setup::details::setup_thread_pools(
+            generate_invalid_no_name_thread_pool()),
+        spdlog_setup::setup_error);
+}
+
+TEST_CASE(
+    "Parse invalid no queue size thread pool",
+    "[parse_invalid_no_queue_size_thread_pool]") {
+    REQUIRE_THROWS_AS(
+        spdlog_setup::details::setup_thread_pools(
+            generate_invalid_no_queue_size_thread_pool()),
+        spdlog_setup::setup_error);
+}
+
+TEST_CASE(
+    "Parse invalid no num threads thread pool",
+    "[parse_invalid_no_num_threads_thread_pool]") {
+    REQUIRE_THROWS_AS(
+        spdlog_setup::details::setup_thread_pools(
+            generate_invalid_no_num_threads_thread_pool()),
+        spdlog_setup::setup_error);
+}
--- src/unit_test/thread_pool.h
+++ src/unit_test/thread_pool.h
@@ -12,6 +12,8 @@
 #include <string>
 #include <utility>
 
+static constexpr auto TEST_THREAD_POOL_NAME = "default";
+
 inline auto generate_global_thread_pool() -> std::shared_ptr<cpptoml::table> {
     namespace names = spdlog_setup::details::names;
 
@@ -19,10 +21,78 @@ inline auto generate_global_thread_pool() -> std::shared_ptr<cpptoml::table> {
     global_thread_inner_pool_table->insert(names::QUEUE_SIZE, size_t(1234));
     global_thread_inner_pool_table->insert(names::NUM_THREADS, size_t(9));
 
-    auto global_thread_pool_table = cpptoml::make_table();
-    global_thread_pool_table->insert(
+    auto conf = cpptoml::make_table();
+    conf->insert(
         names::GLOBAL_THREAD_POOL_TABLE,
         std::move(global_thread_inner_pool_table));
 
-    return std::move(global_thread_pool_table);
+    return std::move(conf);
+}
+
+inline auto generate_simple_thread_pool() -> std::shared_ptr<cpptoml::table> {
+    namespace names = spdlog_setup::details::names;
+
+    auto thread_pool_table_array = cpptoml::make_table_array();
+
+    auto thread_pool_table = cpptoml::make_table();
+    thread_pool_table->insert(names::NAME, TEST_THREAD_POOL_NAME);
+    thread_pool_table->insert(names::QUEUE_SIZE, size_t(1234));
+    thread_pool_table->insert(names::NUM_THREADS, size_t(9));
+    thread_pool_table_array->push_back(std::move(thread_pool_table));
+
+    auto conf = cpptoml::make_table();
+    conf->insert(names::THREAD_POOL_TABLE, std::move(thread_pool_table_array));
+
+    return std::move(conf);
+}
+
+inline auto generate_invalid_no_name_thread_pool()
+    -> std::shared_ptr<cpptoml::table> {
+    namespace names = spdlog_setup::details::names;
+
+    auto thread_pool_table_array = cpptoml::make_table_array();
+
+    auto thread_pool_table = cpptoml::make_table();
+    thread_pool_table->insert(names::QUEUE_SIZE, size_t(1234));
+    thread_pool_table->insert(names::NUM_THREADS, size_t(9));
+    thread_pool_table_array->push_back(std::move(thread_pool_table));
+
+    auto conf = cpptoml::make_table();
+    conf->insert(names::THREAD_POOL_TABLE, std::move(thread_pool_table_array));
+
+    return std::move(conf);
+}
+
+inline auto generate_invalid_no_queue_size_thread_pool()
+    -> std::shared_ptr<cpptoml::table> {
+    namespace names = spdlog_setup::details::names;
+
+    auto thread_pool_table_array = cpptoml::make_table_array();
+
+    auto thread_pool_table = cpptoml::make_table();
+    thread_pool_table->insert(names::NAME, TEST_THREAD_POOL_NAME);
+    thread_pool_table->insert(names::NUM_THREADS, size_t(9));
+    thread_pool_table_array->push_back(std::move(thread_pool_table));
+
+    auto conf = cpptoml::make_table();
+    conf->insert(names::THREAD_POOL_TABLE, std::move(thread_pool_table_array));
+
+    return std::move(conf);
+}
+
+inline auto generate_invalid_no_num_threads_thread_pool()
+    -> std::shared_ptr<cpptoml::table> {
+    namespace names = spdlog_setup::details::names;
+
+    auto thread_pool_table_array = cpptoml::make_table_array();
+
+    auto thread_pool_table = cpptoml::make_table();
+    thread_pool_table->insert(names::NAME, TEST_THREAD_POOL_NAME);
+    thread_pool_table->insert(names::QUEUE_SIZE, size_t(1234));
+    thread_pool_table_array->push_back(std::move(thread_pool_table));
+
+    auto conf = cpptoml::make_table();
+    conf->insert(names::THREAD_POOL_TABLE, std::move(thread_pool_table_array));
+
+    return std::move(conf);
 }
