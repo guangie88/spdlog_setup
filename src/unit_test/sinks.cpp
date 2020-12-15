cb3e8075c3ad7085a636470f45d910ccfe130902
Itay Bookstein <itay.bookstein@nextsilicon.com>
2020-12-15 11:55:09 +0200

    Add tests for dist sink support
    Signed-off-by: Itay Bookstein <itay.bookstein@nextsilicon.com>

--- src/unit_test/sinks.cpp
+++ src/unit_test/sinks.cpp
@@ -41,3 +41,35 @@ TEST_CASE("Parse stderr sink mt", "[parse_generate_stderr_sink_mt]") {
     REQUIRE(typeid(*sink) == typeid(const spdlog::sinks::stderr_sink_mt &));
     REQUIRE(ref_sinks.empty());
 }
+
+TEST_CASE("Parse dist sink st", "[parse_generate_dist_sink_st]") {
+    std::vector<std::string> ref_sinks;
+    const auto sink =
+        spdlog_setup::details::setup_sink(generate_dist_sink_st(), ref_sinks);
+    REQUIRE(typeid(*sink) == typeid(const spdlog::sinks::dist_sink_st &));
+    REQUIRE_THAT(ref_sinks, Catch::Equals<std::string>({"sink1", "sink2"}));
+}
+
+TEST_CASE("Parse dist sink mt", "[parse_generate_dist_sink_mt]") {
+    std::vector<std::string> ref_sinks;
+    const auto sink =
+        spdlog_setup::details::setup_sink(generate_dist_sink_mt(), ref_sinks);
+    REQUIRE(typeid(*sink) == typeid(const spdlog::sinks::dist_sink_mt &));
+    REQUIRE_THAT(ref_sinks, Catch::Equals<std::string>({"sink1", "sink2"}));
+}
+
+TEST_CASE("Parse dist sink circular", "[parse_generate_dist_sink_circular]") {
+    REQUIRE_THROWS_AS(
+        spdlog_setup::details::setup_sinks(generate_dist_sink_circular()),
+        spdlog_setup::setup_error);
+}
+
+TEST_CASE("Parse dist ref sinks", "[parse_generate_dist_sink_refs]") {
+    auto sinks_map =
+        spdlog_setup::details::setup_sinks(generate_dist_sink_with_refs());
+    auto sink = sinks_map["dist_sink"];
+    REQUIRE(typeid(*sink) == typeid(const spdlog::sinks::dist_sink_mt &));
+    // Can't check the dist sink's wiring because there's no getter for its
+    // subordinate sinks, but setup_sinks returning normally rather than
+    // exceptionally is enough
+}
--- src/unit_test/sinks.h
+++ src/unit_test/sinks.h
@@ -44,3 +44,80 @@ inline auto generate_stderr_sink_mt() -> std::shared_ptr<cpptoml::table> {
     sink_table->insert(names::TYPE, std::string("stderr_sink_mt"));
     return std::move(sink_table);
 }
+
+inline auto generate_dist_sink(std::string type)
+    -> std::shared_ptr<cpptoml::table> {
+    namespace names = spdlog_setup::details::names;
+
+    auto ref_sinks = cpptoml::make_array();
+    ref_sinks->push_back(std::string("sink1"));
+    ref_sinks->push_back(std::string("sink2"));
+
+    auto sink_table = cpptoml::make_table();
+    sink_table->insert(names::TYPE, std::move(type));
+    sink_table->insert(names::SINKS, ref_sinks);
+    return std::move(sink_table);
+}
+
+inline auto generate_dist_sink_st() -> std::shared_ptr<cpptoml::table> {
+    return generate_dist_sink("dist_sink_st");
+}
+
+inline auto generate_dist_sink_mt() -> std::shared_ptr<cpptoml::table> {
+    return generate_dist_sink("dist_sink_mt");
+}
+
+inline auto generate_dist_sink_circular() -> std::shared_ptr<cpptoml::table> {
+    namespace names = spdlog_setup::details::names;
+
+    auto dist_sink1 = cpptoml::make_table();
+    dist_sink1->insert(names::NAME, std::string("sink1"));
+    dist_sink1->insert(names::TYPE, std::string("dist_sink_st"));
+    auto dist_sink1_sinks = cpptoml::make_array();
+    dist_sink1_sinks->push_back(std::string("sink2"));
+    dist_sink1->insert(names::SINKS, dist_sink1_sinks);
+
+    auto dist_sink2 = cpptoml::make_table();
+    dist_sink2->insert(names::NAME, std::string("sink2"));
+    dist_sink2->insert(names::TYPE, std::string("dist_sink_mt"));
+    auto dist_sink2_sinks = cpptoml::make_array();
+    dist_sink2_sinks->push_back(std::string("sink1"));
+    dist_sink2->insert(names::SINKS, dist_sink2_sinks);
+
+    auto sink_table = cpptoml::make_table_array();
+    sink_table->push_back(dist_sink1);
+    sink_table->push_back(dist_sink2);
+
+    auto config = cpptoml::make_table();
+    config->insert(names::SINK_TABLE, sink_table);
+    return std::move(config);
+}
+
+inline auto generate_dist_sink_with_refs() -> std::shared_ptr<cpptoml::table> {
+    namespace names = spdlog_setup::details::names;
+
+    auto dist_sink = cpptoml::make_table();
+    dist_sink->insert(names::NAME, std::string("dist_sink"));
+    dist_sink->insert(names::TYPE, std::string("dist_sink_mt"));
+    auto ref_sinks = cpptoml::make_array();
+    ref_sinks->push_back(std::string("sink1"));
+    ref_sinks->push_back(std::string("sink2"));
+    dist_sink->insert(names::SINKS, ref_sinks);
+
+    auto sink1 = cpptoml::make_table();
+    sink1->insert(names::NAME, std::string("sink1"));
+    sink1->insert(names::TYPE, std::string("null_sink_mt"));
+
+    auto sink2 = cpptoml::make_table();
+    sink2->insert(names::NAME, std::string("sink2"));
+    sink2->insert(names::TYPE, std::string("null_sink_mt"));
+
+    auto sink_table = cpptoml::make_table_array();
+    sink_table->push_back(dist_sink);
+    sink_table->push_back(sink1);
+    sink_table->push_back(sink2);
+
+    auto config = cpptoml::make_table();
+    config->insert(names::SINK_TABLE, sink_table);
+    return std::move(config);
+}
